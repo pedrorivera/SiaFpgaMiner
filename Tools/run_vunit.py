d99eaf9cbbf7125f2c4dffba5c021d848fc56ae7
jlloyola <jlloyola@users.noreply.github.com>
2018-02-05 23:12:58 -0600

    First steps to use VUnit in the project.

---
+++ Tools/run_vunit.py
@@ -0,0 +1,20 @@
+import os
+
+from vunit import VUnit
+
+# Create VUnit instance by parsing command line arguments
+vu = VUnit.from_argv()
+
+# Create library 'lib'
+lib = vu.add_library("lib")
+
+# Add all files ending in .vhd in the following directories
+dirs = [".", "Example", "Testbench"]
+script_path = os.path.dirname(__file__)
+dirs = [os.path.realpath(
+    os.path.join(script_path, "..", d, "*.vhd")) for d in dirs]
+for d in dirs:
+    lib.add_source_files(d)
+
+# Run vunit function
+vu.main()
