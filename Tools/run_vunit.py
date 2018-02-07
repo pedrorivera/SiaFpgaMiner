33c98b595e9731f5094a53eaf88faf8951a83f3f
jlloyola <jlloyola@users.noreply.github.com>
2018-02-06 21:34:11 -0600

    Skipping Example folder due to Xilinx library dependency.

--- Tools/run_vunit.py
+++ Tools/run_vunit.py
@@ -9,7 +9,8 @@ vu = VUnit.from_argv()
 lib = vu.add_library("lib")
 
 # Add all files ending in .vhd in the following directories
-dirs = [".", "Example", "Testbench"]
+# dirs = [".", "Example", "Testbench"]
+dirs = [".", "Testbench"]
 script_path = os.path.dirname(__file__)
 dirs = [os.path.realpath(
     os.path.join(script_path, "..", d, "*.vhd")) for d in dirs]
